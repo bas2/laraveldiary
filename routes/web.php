4f4263de5f484b393f535753fc8b8028bec72845
bas2 <me@bashir.biz>
2017-02-21 18:29:00 +0000

    Created new dates variables for full day of week and selected day. Changed variables for previous and next week from the selected date +/- 7 days. iscurwk variable to indicate whether selected date is in previous, this or next week. Get more data from entries table for display. Checks on data from database to see whether they're empty so that default values can be pass (since an error is returned). Pass values in json format now. Date entries and tooltip texts are passed in separate foreach loops. Added code to calendar route: days in selected month. blankcells variable for adding blank cells before 1st day of month. Route added for quickentries. Check on info to make sure it's empty in which case it's set to an empty string. Finally increment change number.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('home', function () {
 
 Route::get('ajax/getday/{date}', function($date){
   $date2 = \Carbon\Carbon::parse($date);
+  $date3 = $date2->copy()->format('l');
+  $date4 = $date2->copy();
   $mon = $date2->startOfWeek();
   $tue = $mon->copy()->addDay();
   $wed = $tue->copy()->addDay();
@@ -25,31 +27,72 @@ Route::get('ajax/getday/{date}', function($date){
   $sat = $fri->copy()->addDay();
   $sun = $sat->copy()->addDay();
 
-  $prvwk = $mon->copy()->subDays(7)->format('Y-m-d');
-  $nxtwk = $mon->copy()->addDays(7)->format('Y-m-d');
-  
+  $prvwk = $date4->copy()->subDays(7)->format('Y-m-d');
+  $nxtwk = $date4->copy()->addDays(7)->format('Y-m-d');
+
+  $curwk = \Carbon\Carbon::now()->startOfWeek()->format('Y-m-d');
+
+  $dateisthisweek=($curwk==$date2->copy()->format('Y-m-d')) ? 1 : 0 ;
+  $datesnextisthisweek=($curwk==$nxtwk) ? 1 : 0 ;
+  $datesprevisthisweek=($curwk==$prvwk) ? 1 : 0 ;
+
+  $curwk="{$datesprevisthisweek}-{$dateisthisweek}-{$datesnextisthisweek}";
+
   $count=App\Entry::count();
-  $entries=App\Entry::where('d',$date)->get(['info']);
-  $info=(empty($entries[0]->info)) ? '' : $entries[0]->info ;
-  $str = "{$info}-||-Imp-||-1990-01-01-||-Ch-||-Editnum-||-{$count}-||-1-||-22-||-3-||-4-||-5-||-6-||-7-||-otd13-||-imp14-||-15?-||-{$prvwk}-||-{$nxtwk}";
+  $entries=App\Entry::where('d',$date)->get(['info','created_at','updated_at','numedit','important']);
+  $info=(empty($entries[0]->info))       ? '' : $entries[0]->info ;
+  $crdt=(empty($entries[0]->created_at)) ? '' : $entries[0]->created_at->format('d/m/Y H:i') ;
+  $chdt=(empty($entries[0]->updated_at)) ? '' : $entries[0]->updated_at->format('d/m/Y H:i') ;
+  $nume=(empty($entries[0]->numedit))    ? '0' : $entries[0]->numedit ;
+  $impo=(empty($entries[0]->important))  ? '' : $entries[0]->important ;
+
+  $json=[$info,'Imp',$crdt,$chdt,$nume,$count];
 
   foreach ([$mon,$tue,$wed,$thu,$fri,$sat,$sun] as $value) {
     $entries=App\Entry::where('d',$value)->get(['info']);
     $info=(empty($entries[0]->info)) ? '' : $entries[0]->info ;
-    $str.= "-||-<a class='datehead' tooltiptxt='{$info}' title3='{$value->format('Y-m-d')}' title4='{$value->format('D')}' iscurwk='1'>{$value->format('l jS F')}</a>";
+    $json[]=$info;
+  }
+  foreach(['otd13','imp14',$date3,$prvwk,$nxtwk] as $element) {$json[]=$element;}
+
+  foreach ([$mon,$tue,$wed,$thu,$fri,$sat,$sun] as $value) {
+    $entries=App\Entry::where('d',$value)->get(['info']);
+    $info=(empty($entries[0]->info)) ? '' : $entries[0]->info ;
+    $info=str_replace("'", '&lsquo;', $info);
+    $json[]= "<a class='datehead' tooltiptxt='{$info}' title3='{$value->format('Y-m-d')}' title4='{$value->format('D')}' iscurwk='1'>{$value->format('l jS F')}</a>";
+  }
+  foreach(['Forth25',$curwk,'c','n'] as $element) {$json[]=$element;}
+  $json=json_encode($json);
+  return $json;
+});
+
+
+Route::get('ajax/calendar/{date}', function($date){
+  $seldatest=\Carbon\Carbon::parse($date)->startOfMonth();
+  $seldateen=\Carbon\Carbon::parse($date)->endOfMonth();
+
+  $firstdayofmonth = $seldatest->format('D');
+  $daysinmonth     = $seldatest->daysInMonth;
+
+  $blankcells=0;
+  foreach (['Tue'=>1,'Wed'=>2,'Thu'=>3,'Fri'=>4,'Sat'=>5,'Sun'=>6] as $day=>$blankcells1) {
+    if($firstdayofmonth==$day) {$blankcells=$blankcells1;}
   }
 
-  //$str.="-||-{$tue->format('D d/m/Y')}-||-{$wed->format('D d/m/Y')}-||-{$thu->format('D d/m/Y')}-||-{$fri->format('D d/m/Y')}-||-{$sat->format('D d/m/Y')}-||-{$sun->format('D d/m/Y')}";
+  $monthentries=App\Entry::whereBetween('d',[$seldatest->format('Y-m-d'),$seldateen->format('Y-m-d')])->get(['d','info']);
+  $me2=[];foreach($monthentries as $me3) {
+    $d=\Carbon\Carbon::parse($me3->d)->format('j');
+    if (!empty($me3->info)) {
+      $me2[$d]=$me3->info;
+    } else {$me2[$d]='';}
+  }
+  
 
-  $str.= "-||-Forth25-||-p-||-c-||-n";
-
-  //return view('getday')->with('date', $date)->with('content', $str);
-  return $str;
+  return view('ajax.calendar')->with('vals',
+    ['daysinmonth'=>$daysinmonth,'monthentries'=>$me2,'blankcells'=>$blankcells,'seldate'=>$seldatest]
+  );
 });
 
-Route::get('ajax/calendar', function(){
-  return view('ajax.calendar');
-});
 
 Route::get('ajax/images', function(){
 
@@ -59,8 +102,34 @@ Route::post('ajax/related', function(){
 
 });
 
+Route::get('ajax/quickentries/{mode}', function($mode){
+
+  $mode2 = 'a';
+  if ($mode=='u') { // Update mode.
+    $mode2 = 'u';
+  } else if ($mode=='d') { // Delete mode.
+    $mode2 = 'd';
+  } // End if.
+
+  $addclass = ($mode2=='a')  ? ' highlight' : '';
+  $updclass = ($mode2=='u')  ? ' highlight' : '';
+  $delclass = ($mode2=='d')  ? ' highlight' : '';
+  $upclass  = ($mode2=='up') ? ' highlight' : '';
+
+  $qentries=\App\Qentry::orderBy('id')->get(['id','text']);
+
+  return view('ajax.quickentries')->with('mode',$mode2)->with('classes',[$addclass,$updclass,$delclass,$upclass])->with('data',$qentries);
+});
+
+Route::get('ajax/quickentries/up/{id}', function($id){
+
+});
+
+
 Route::post('ajax/update/{date}', function($date){
   $input=Request::all();
-  $videos = new \App\Entry;
-  $videos->updateOrInsert(['d'=>$date], ['info'=>$input['info']]);
+  $videos=new \App\Entry;
+  $info=(empty($input['info'])) ? '' : $input['info'] ;
+  $videos->updateOrInsert(['d'=>$date], ['info'=>$info]);
+  $videos=new \App\Entry;$videos->where('d',$date)->increment('numedit');
 });
