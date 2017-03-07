7c1b157af37b94b436ac76642dc1ba853bbd9ad5
bas2 <me@bashir.biz>
2017-03-07 19:36:29 +0000

    Added daysdiff method to help with calendar.

--- app/Entry.php
+++ app/Entry.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entry extends Model
 {
-    //
+  public static function daysdiff($date) { 
+    return (\Carbon\Carbon::parse($date)->diff(\Carbon\Carbon::now())->days < 1) ? 'today' 
+    : \Carbon\Carbon::parse($date)->startOfDay()->diffForHumans(\Carbon\Carbon::now()->startOfDay());
+  }
 }
