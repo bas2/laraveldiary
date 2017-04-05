9f98c67fadd13fb7138ab62547e88f6f7805f352
bas2 <me@bashir.biz>
2017-04-05 20:57:56 +0100

    New commit. Added activities method to get activity name.

---
+++ app/DailyActivity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DailyActivity extends Model
+{
+  public function activities() {
+    return $this->hasMany(Activity::class, 'id', 'activityid');
+  }
+}
