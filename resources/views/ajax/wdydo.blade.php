10564aa557700c4052d1c1e227842848738ed8af
bas2 <me@bashir.biz>
2017-04-28 23:58:22 +0100

    Added cancel button.  Added  name attributes to both Add and Cancel.

--- resources/views/ajax/wdydo.blade.php
+++ resources/views/ajax/wdydo.blade.php
@@ -5,5 +5,6 @@
 {{ Form::text('whatdidyoudot',null,['class'=>'form-control']) }}
 </div>
 <div class="form-group">
-{{ Form::button('Add >',['class'=>'btn btn-primary btn-block']) }}
+{{ Form::button('Add >',['name'=>'add','class'=>'btn btn-primary btn-block']) }}
+{{ Form::button('Cancel >',['name'=>'cancel','class'=>'btn btn-warning btn-block']) }}
 </div>
