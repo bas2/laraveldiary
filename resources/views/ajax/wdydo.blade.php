8dea7e3808accce6989d53da383d9d5da8380cc4
bas2 <me@bashir.biz>
2017-05-02 20:07:30 +0100

    Activity image list and jQuery to handle their click events.

--- resources/views/ajax/wdydo.blade.php
+++ resources/views/ajax/wdydo.blade.php
@@ -1,3 +1,9 @@
+<ul class="list-inline">
+  @foreach($activities as $activityid=>$activity)
+  <li>{{ Html::image("img/activities/$activityid.jpg",$activityid,['width'=>80,'title'=>$activity,'class'=>'thumbnail']) }}</li>
+  @endforeach
+</ul>
+
 <div class="form-group">
 {{ Form::select('whatdidyoudoa',$activities,'',['class'=>'form-control']) }}
 </div>
@@ -8,3 +14,18 @@
 {{ Form::button('Add >',['name'=>'add','class'=>'btn btn-primary btn-block']) }}
 {{ Form::button('Cancel >',['name'=>'cancel','class'=>'btn btn-warning btn-block']) }}
 </div>
+<script>
+//$(document).ready(function(){
+  $('body').on('click','.activitysel li img',function(){
+    $('.activitysel select[name=whatdidyoudoa]').val($(this).attr('alt'));
+        var $this=$(this);
+    $.ajax({
+      type: 'get',
+      url: 'activityhint/' + $this.attr('alt') ,
+      success: function (hint){
+        $this.parent().parent().next().next().find('input[name=whatdidyoudot]').val('').attr('placeholder',hint).focus();
+      }
+    });
+  });
+//});
+</script>
